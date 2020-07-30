c34cb47ddb1543cc07539fa765b8213bf82c6e48
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-30 10:53:07 +0200

    AddItemToArrayInObject

---
+++ Datastructure/modifyObjectArray.js
@@ -0,0 +1,29 @@
+/*
+Add new friend to array of friends in object
+*/
+
+let user = {
+	name: "Kenneth",
+	age: 28,
+	data: {
+		username: "kennethCodesAllDay",
+		joinDate: "March 26, 2016",
+		organization: "freeCodeCamp",
+		friends: ["Sam", "Kira", "Tomo"],
+		location: {
+			city: "San Francisco",
+			state: "CA",
+			country: "USA",
+		},
+	},
+}
+
+function addFriend(userObj, friend) {
+	// Only change code below this line
+	let refFriends = userObj.data.friends
+	let newListOfFriends = [...newObject, friend]
+	return (refFriends = newListOfFriends)
+	// Only change code above this line
+}
+
+console.log(addFriend(user, "Pete"))
