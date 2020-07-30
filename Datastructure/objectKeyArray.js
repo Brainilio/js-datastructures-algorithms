ad0ca23167fc80ab20fab8a0ad271328d3649213
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-30 09:39:27 +0200

    objectkeyarray

---
+++ Datastructure/objectKeyArray.js
@@ -0,0 +1,31 @@
+/* 
+Generate an array out of object's keys:
+*/
+
+let users = {
+	Alan: {
+		age: 27,
+		online: false,
+	},
+	Jeff: {
+		age: 32,
+		online: true,
+	},
+	Sarah: {
+		age: 48,
+		online: false,
+	},
+	Ryan: {
+		age: 19,
+		online: true,
+	},
+}
+
+function getArrayOfUsers(obj) {
+	// Only change code below this line
+	let arrayOfKeys = Object.keys(obj)
+	return arrayOfKeys
+	// Only change code above this line
+}
+
+console.log(getArrayOfUsers(users))
