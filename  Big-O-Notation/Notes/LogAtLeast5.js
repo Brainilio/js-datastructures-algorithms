003735b385a41a226f37ecc38093367108e1a97e
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-08 21:54:35 +0200

    big o notation

---
+++  Big-O-Notation/Notes/LogAtLeast5.js
@@ -0,0 +1,4 @@
+function logAtLeast5(n) {
+	for (var i = 1; i <= Math.max(5, n); i++) {}
+	console.log(i)
+}
---  Big-O-Notation/PrintAllpairs.js
+++  Big-O-Notation/PrintAllpairs.js
@@ -11,6 +11,5 @@ first loop based on N so O(N)
 second loop is also based on N right? O(N) 
 o(n) operation inside of an o(n) operation = O(N^2)
 If n = 3 the second pair will be 3 x 3 so 9 pairs in total 
-if n = 5 all the pairs wil be 25
-
+if n = 5 all the pairs wil be 25\
 */
