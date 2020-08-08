52514d41a819e7812d66c7c94d6e10c098346393
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-08 21:55:51 +0200

    CHECK BIG O of logatleast 5

---  Big-O-Notation/Notes/LogAtLeast5.js
+++  Big-O-Notation/Notes/LogAtLeast5.js
@@ -2,3 +2,11 @@ function logAtLeast5(n) {
 	for (var i = 1; i <= Math.max(5, n); i++) {}
 	console.log(i)
 }
+
+/* 
+We have a loop: goes from 1 to 5 or either N;
+If we worry about 5, only if N is small ( under 5) 
+But what happens if N is 1 million? Then this loop is going to 
+run 1 million time 
+If N grows, the number grows so: O(N)
+*/
