a68cffcb9b79fcc0e5ba3f472bee4eaab1662f3c
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-12 17:47:02 +0200

    sum zero

--- Algorithm/masterclass/multiple-pointers-pattern/sumzero.js
+++ Algorithm/masterclass/multiple-pointers-pattern/sumzero.js
@@ -17,3 +17,22 @@ Output: undefined
 
 
 */
+
+//Using multiple pointers
+//Time complexity - O(N)
+//Space complexity - O(1)
+
+function sumZero(arr) {
+	let left = 0
+	let right = arr.length - 1
+	while (left < right) {
+		let sum = arr[left] + arr[right]
+		if (sum === 0) {
+			return [arr[left], arr[right]]
+		} else if (sum > 0) {
+			right--
+		} else {
+			left++
+		}
+	}
+}
