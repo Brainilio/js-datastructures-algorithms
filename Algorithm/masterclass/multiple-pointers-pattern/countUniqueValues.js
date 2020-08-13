d52f790b1bcc111cddb8c967fb22d93500a57151
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-13 10:20:37 +0200

    count unique value

--- Algorithm/masterclass/multiple-pointers-pattern/countUniqueValues.js
+++ Algorithm/masterclass/multiple-pointers-pattern/countUniqueValues.js
@@ -19,9 +19,34 @@ Output: 4
 */
 
 function countUniqueValues(arr) {
+	//if array length is zero or has nothing in it at all, return 0
 	if (arr.length <= 0) {
 		return 0
 	}
-	let firstPointer = 0
-	let secondPointer = 1
+
+	//left pointer
+	let left = 0
+	//right pointer
+	let right = 1
+
+	//while right is not at the end of the array index
+	while (right < arr.length) {
+		//if arr[1] is equal to arr[0] you want to move right because its not an unique value
+		if (arr[right] === arr[left]) {
+			right++
+		}
+
+		//however if youe ncounter a unique value, move the pointer up first
+		// and then replace the number at arr[left] with arr[right]
+		if (arr[left] !== arr[right]) {
+			left++
+			arr[left] = arr[right]
+		}
+	}
+
+	//left is basically on the count of where the last unique value in the
+	//array is, so you can return left
+	return left
 }
+
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7]))
