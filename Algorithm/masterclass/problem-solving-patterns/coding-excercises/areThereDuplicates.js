2c0026306a4161e861f3af384c4a00b1e5792296
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-13 11:52:43 +0200

    2 DIFF solutions

--- Algorithm/masterclass/problem-solving-patterns/coding-excercises/areThereDuplicates.js
+++ Algorithm/masterclass/problem-solving-patterns/coding-excercises/areThereDuplicates.js
@@ -36,3 +36,26 @@ function areThereDuplicates() {
 
 	return false
 }
+
+//multiple pointer solution
+
+function areThereDuplicates(...args) {
+	// Two pointers
+	args.sort((a, b) => a > b)
+	let start = 0
+	let next = 1
+	while (next < args.length) {
+		if (args[start] === args[next]) {
+			return true
+		}
+		start++
+		next++
+	}
+	return false
+}
+
+// one liner
+
+function areThereDuplicates() {
+	return new Set(arguments).size !== arguments.length
+}
