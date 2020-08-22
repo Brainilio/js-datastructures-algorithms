d09a5bb090efd18651b223782b86f852c59cf5da
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-22 13:11:50 +0200

    sinngle linked list

--- masterclass/datastructures/singly-linked-list/singly-linked-list.js
+++ masterclass/datastructures/singly-linked-list/singly-linked-list.js
@@ -14,6 +14,31 @@ class SinglyLinkedList {
 		this.tail = 0
 		this.length = 0
 	}
+	push(val) {
+		//grab current tail to have val as value now
+		//create new node using the value passed to the function
+		//if there is no head property on the list, set the head and tail to be the newly created node
+		//if head = set next property on the tail to be new node, and set tail property to be new node
+		//increment length by one
+		let newNode = new Node(val)
+		if (this.head) {
+			this.head = newNode
+			this.tail = newNode
+		} else {
+			this.tail.next = newNode
+			this.tail = newNode
+		}
+		thislength += 1
+		return this
+	}
+	traverse() {
+		let current = this.head
+		while (current) {
+			console.log(current.val)
+			current = current.next
+		}
+	}
+	pop() {}
 }
 
 //base list
