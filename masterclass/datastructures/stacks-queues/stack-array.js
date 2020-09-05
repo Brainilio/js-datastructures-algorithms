05bcd855be599c005f68d06fc947af0401769c04
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-09-05 19:12:26 +0200

    using array as a stack

--- masterclass/datastructures/stacks-queues/stack-array.js
+++ masterclass/datastructures/stacks-queues/stack-array.js
@@ -7,3 +7,14 @@ stack.push("Page three")
 
 //removing from top of the stack;
 stack.pop()
+
+//Using any other method would be an anti-pattern...
+
+// BAD! Time complexity of unshift/shift...
+
+// add to beginning
+stack.unshift("create new file")
+stack.unshift("resized file")
+
+//remove from the beginning
+stack.shift()
