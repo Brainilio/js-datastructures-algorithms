ea882f8f890c72fa54af969663545c1d7657456c
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-09-17 10:52:21 +0200

    graph traversing BFS, dfs iterative & recursive

--- masterclass/datastructures/graph/graph.js
+++ masterclass/datastructures/graph/graph.js
@@ -54,21 +54,161 @@ class Graph {
 		}
 		delete this.adjacencyList[vertex]
 	}
+	DFSRecursive(vertex) {
+		/* RECURSIVE SOLUTION:
+          if vertex is empty return (this is the base case) 
+          add vertex to results list 
+          mark vertex as visited (key value pair in an object and set it to true)
+          for each neighbor in vertex's neighbors: 
+               if neighbor is not visited: 
+               recursively call dfs on neighbor
+
+          The function should accept a starting node
+          Create a list to store the end result, to be returned at the very end 
+          Create an object to store your visited vertices 
+          Create a helper function which accepts a vertex
+               - The helper function should return early if the vertex is empty
+               - The helper function should place the vertex it accepts into the
+               visited object and pusht hat vertex into the result array. 
+               - Loop over all of the values in the adjacencylist for that vertex
+               - If any of those values have not been visited, recursively invoke
+               the helper function with that vertex
+          */
+		let endResult = []
+		let visitedVertices = {}
+		const adjacency = this.adjacencyList
+
+		function helperFunction(v) {
+			if (!v) return null
+
+			let vertexWithValues = adjacency[v]
+
+			visitedVertices[v] = true
+			endResult.push(v)
+
+			vertexWithValues.map((neighbor) => {
+				if (!visitedVertices[neighbor]) {
+					return helperFunction(neighbor)
+				} else return null
+			})
+		}
+
+		helperFunction(vertex)
+
+		return endResult
+	}
+	DFSIterative(vertex) {
+		/*   ITERATIVE SOLUTION 
+          
+          -- the order is different because: you pop off the end of the array that you 
+          add to the stack so if you push in A and A's neighbors are [B, C] and you use
+          .pop() you get the end of the array, -> C. 
+
+               S = stack
+               s.push(start) 
+               while S is not empty 
+               vertex = s.pop() 
+               if vertex is not labeled as discovered: 
+                    visit vertex (add to result list) 
+                    label vertex as discovered
+                    for each of vertex's neighbors, n do s.push(n)
+
+               The function should accept a starting node
+               - Create a stack to help use keep track of vertices (list/array)
+               - Create a list to store the end result, to be returned at the very end
+               - Create an object to store visited vertices
+               - Add the starting vertex to the stack, and mark it visited
+               - While the stack has something in it: 
+                    - Pop the next vertex from the stack
+                    - If that vertex hasn't been visited yet: 
+                         - Mark it as visited
+                         - Add it to the result list
+                         - Push all of its neighbors into the stack
+          */
+		let stack = [vertex]
+		let endResult = []
+		let visitedVertices = {}
+		let poppedVertex
+
+		visitedVertices[vertex] = true
+
+		while (stack.length) {
+			poppedVertex = stack.pop()
+			endResult.push(poppedVertex)
+
+			this.adjacencyList[poppedVertex].map((neighbor) => {
+				if (!visitedVertices[neighbor]) {
+					visitedVertices[neighbor] = true
+					stack.push(neighbor)
+				}
+			})
+		}
+
+		return endResult
+	}
+	BFS(vertex) {
+		/*    
+          - Accept a vertex
+          - Create a queue (you can use an array) and place the starting vertex in it
+          - Create an array to store the nodes visited
+          - Create an object to store nodes visited
+          - mark the starting vertex as visited
+          - Loop as long as there is anything in the queue 
+          - Remove the first vertex from the queue and push it into the array 
+          that stores the nodes visited
+          - Loop over each vertex in the adjacency list for the vertex you are visitng. 
+          - If it is not inside the objcet that stores nodes visited, mark it as 
+          visited and enqueue that vertex 
+          - Once you have finished looping, return the array of visited nodes. 
+          */
+
+		let queue = [vertex]
+		let endResult = []
+		let visitedVertices = {}
+		let current
+
+		visitedVertices[vertex] = true
+
+		while (queue.length) {
+			current = queue.shift()
+			endResult.push(current)
+
+			this.adjacencyList[current].map((neighbor) => {
+				if (!visitedVertices[neighbor]) {
+					visitedVertices[neighbor] = true
+					queue.push(neighbor)
+				}
+			})
+		}
+
+		return endResult
+	}
 }
 
 //test cases:
 let graph = new Graph()
-graph.addVertex("Dallas")
-graph.addVertex("Tokyo")
-graph.addVertex("Aspen")
-graph.addVertex("Los Angeles")
-graph.addVertex("Hong Kong")
-graph.addEdge("Dallas", "Tokyo")
-graph.addEdge("Dallas", "Aspen")
-graph.addEdge("Hong Kong", "Tokyo")
-graph.addEdge("Hong Kong", "Dallas")
-graph.addEdge("Los Angeles", "Hong Kong")
-graph.addEdge("Los Angeles", "Aspen")
-console.log(graph.adjacencyList)
-graph.removeVertex("Tokyo")
-console.log(graph.adjacencyList)
+graph.addVertex("A")
+graph.addVertex("B")
+graph.addVertex("C")
+graph.addVertex("D")
+graph.addVertex("E")
+graph.addVertex("F")
+graph.addEdge("A", "B")
+graph.addEdge("A", "C")
+graph.addEdge("B", "D")
+graph.addEdge("C", "E")
+graph.addEdge("D", "E")
+graph.addEdge("D", "F")
+graph.addEdge("E", "F")
+
+// GRAPH We're visiting:
+//              A
+//          /      \
+//         B        C
+//          \       /
+//           D --- E
+//           \     /
+//              F
+console.group(graph.DFSRecursive("A")) // [A, B, D, E, C, F]
+console.log(graph.DFSIterative("A")) // [A, C, E, F, D, B]
+console.log(graph.BFS("A")) // [A, B, C, D, E, F]
